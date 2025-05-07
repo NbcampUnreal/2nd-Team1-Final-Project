c74d279e91d42104003ad489badfa9fb4b9da5fd
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-05-07 19:20:43 +0900

    [Add] 던전에서 베이스가 되는 캐릭터 클래스 추가

---
+++ Source/RogShop/Character/RSDunBaseCharacter.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "RSDunBaseCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGSHOP_API ARSDunBaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float WalkSpeed;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, category = "Stat", meta = (AllowPrivateAccess = "true"))
+	float MaxHP;
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, category = "Stat", meta = (AllowPrivateAccess = "true"))
+	float HP;
+};
