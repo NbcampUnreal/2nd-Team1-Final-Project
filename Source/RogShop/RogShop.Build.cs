d362111a93cfe731c7c8bbbee9845f20763f6a17
yeong132 <pfyrmog6964@gmail.com>
2025-05-15 11:59:15 +0900

    [Refac] PR 피드백 요청에 따른 폴더 이름 변경 AI->Monster, build.cs에 경로도 변경

--- Source/RogShop/RogShop.Build.cs
+++ Source/RogShop/RogShop.Build.cs
@@ -12,7 +12,7 @@ public class RogShop : ModuleRules
 
         PublicIncludePaths.AddRange(new string[] {
             Path.Combine(ModuleDirectory, "Character"),
-            Path.Combine(ModuleDirectory, "Character", "AI"),
+            Path.Combine(ModuleDirectory, "Character", "Monster"),
             Path.Combine(ModuleDirectory, "Controller"),
             Path.Combine(ModuleDirectory, "Widget", "DunShop"),
             Path.Combine(ModuleDirectory, "AnimInstances"),
@@ -22,7 +22,6 @@ public class RogShop : ModuleRules
             Path.Combine(ModuleDirectory, "GameInstance"),
             Path.Combine(ModuleDirectory, "Actor", "Dungeon", "Weapon"),
             Path.Combine(ModuleDirectory, "AnimNotifyState"),
-            Path.Combine(ModuleDirectory, "ActorComponent"),
             Path.Combine(ModuleDirectory, "Interface"),
         });
 
