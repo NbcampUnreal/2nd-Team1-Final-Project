ddac12f5ec208ee42fdde62fd524b406d95ca2cf
yeong132 <pfyrmog6964@gmail.com>
2025-05-30 21:18:49 +0900

    [Fix] 몬스터 데이터 테이블에서 닭 이동 속도 정상으로 복구, 플레이어->몬스터 공격판정 되도록 오버랩 이벤트 활성화

--- Source/RogShop/DataTable/MonsterData.h
+++ Source/RogShop/DataTable/MonsterData.h
@@ -14,6 +14,54 @@ enum class EMonsterType : uint8
 	Boss,
 };
 
+//USTRUCT(BlueprintType)
+//struct FMonsterAttackTraceData
+//{
+//	GENERATED_BODY()
+//
+//public:
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+//	FVector TraceBoxHalfSize;	// 트레이스 반경 (옆으로 얼마만큼 쏠지)
+//
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+//	float TraceLength;			// 트레이스 길이 (앞으로 얼마만큼 쏠지)
+//
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+//	float TraceForwardOffset;	// 소켓 시작점 앞뒤 보정
+//
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+//	float TraceRightOffset;     // 소켓 시작점 좌우 보정
+//
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+//	float TraceUpOffset;        // 소켓 시작점 높이 보정
+//
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+//	FName SocketLocation;		// 트레이스가 쏴질 소켓의 시작점
+//
+//};
+//
+//USTRUCT(BlueprintType)
+//struct FMonsterAttackSkillData
+//{
+//	GENERATED_BODY()
+//
+//public:
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+//	FName SkillName;
+//
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+//	TObjectPtr<UAnimMontage> SkillMontage;
+//
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+//	int32 PlayRate;
+//
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+//	float Damage;
+//
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+//	FMonsterAttackTraceData AttackTrace;
+//};
+
 USTRUCT(BlueprintType)
 struct ROGSHOP_API FMonsterData : public FTableRowBase
 {
@@ -25,6 +73,9 @@ public:
 
 	UPROPERTY(EditDefaultsOnly)
 	FText Description;
+
+	/*UPROPERTY(EditDefaultsOnly)
+	TArray<FMonsterAttackSkillData> MonsterAttackSkills;*/
 
 	UPROPERTY(EditDefaultsOnly)
 	float MaxHP;
