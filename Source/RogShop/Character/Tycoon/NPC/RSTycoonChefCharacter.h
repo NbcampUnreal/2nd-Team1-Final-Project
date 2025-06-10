9edc4f6f9ec575aecc06d190afebde48d63d4df5
KimYoungWung <yyo0033@naver.com>
2025-06-10 14:29:25 +0900

    [Add] Customer와 Chef에 FoodBubble을 추가
    현재 시킨 음식과 현재 제작중인 음식을 알 수 있는 FoodBubble을 추가했습니다

--- Source/RogShop/Character/Tycoon/NPC/RSTycoonChefCharacter.h
+++ Source/RogShop/Character/Tycoon/NPC/RSTycoonChefCharacter.h
@@ -6,6 +6,7 @@
 #include "RSTycoonNPC.h"
 #include "RSTycoonChefCharacter.generated.h"
 
+class UWidgetComponent;
 class ARSCookingTile;
 class ARSTileMap;
 
@@ -24,13 +25,16 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-	
 	virtual void InteractTarget(AActor* TargetActor) override;
 
 private:
 	void TryCook();
 	void CheckFinish();
-	
+
+protected:
+	UPROPERTY(EditDefaultsOnly)	
+	TObjectPtr<UWidgetComponent> FoodBubbleWidgetComponent;
+
 private:
 	UPROPERTY()
 	TObjectPtr<ARSCookingTile> PlacedCookingTile; //배치된 쿠킹 타일
