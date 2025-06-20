e2103f22af049d187a05e2f9a278cb730d14a7a6
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-06-20 20:04:01 +0900

    [Add] 플레이어 HPBar 구현

---
+++ Source/RogShop/Widget/Dungeon/RSPlayerHPBarWidget.h
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RSPlayerHPBarWidget.generated.h"
+
+class UTextBlock;
+class UHorizontalBox;
+class USizeBox;
+class UProgressBar;
+class UImage;
+
+UCLASS()
+class ROGSHOP_API URSPlayerHPBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeOnInitialized() override;
+
+	virtual void NativeConstruct() override;
+
+private:
+	UFUNCTION()
+	void UpdateHP(float HP, float MaxHP);
+
+// 마지막 업데이트의 현재 체력과 최대 체력 캐싱
+private:
+	float LastUpdateHP;
+
+	float LastUpdateMaxHP;
+
+private:
+	// HP의 숫자 텍스트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<UTextBlock> MaxHPText;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<UTextBlock> HPText;
+
+	// HP 바
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<UHorizontalBox> HPBarHorizontalBox;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<USizeBox> HPBarSizeBox;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<UProgressBar> HPProgressBar;
+
+	// HP 바의 마커
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<UHorizontalBox> MarkerHorizontalBox;
+
+private:
+	// 마커
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UTexture2D> MarkerS;	// 마커에서 사용할 텍스처
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UTexture2D> MarkerL;	// 마커에서 사용할 텍스처
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TArray<TObjectPtr<UImage>> MarkerArray;	// 생성한 마커 목록
+};
